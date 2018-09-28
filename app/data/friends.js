cc2ae908819d89b8d97563b47fcd48e63c98fa45
David Huh <davidmhuh@gmail.com>
2018-09-28 05:35:49 -0700

    friendfinder

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friends = [
+    {
+        name: "Cartman",
+        photo: "https://i.ytimg.com/vi/fLsvvF9HjqA/hqdefault.jpg",
+        scores: [
+            "1",
+            "3",
+            "5",
+            "1",
+            "3",
+            "5",
+            "1",
+            "3",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: "Randy",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyz0kltyOd7PPwNQuU4SDv-3NcuKBcWWvJPxsSRj7IDKMXqNC_",
+        scores: [
+            "2",
+            "4",
+            "5",
+            "3",
+            "4",
+            "1",
+            "2",
+            "5",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Stan",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuQIImrUhgAcu3N80ntjjRVdb41OnQXgO7Xzx0DgHxh6rgOIvW",
+        scores: [
+            "4",
+            "5",
+            "3",
+            "1",
+            "4",
+            "2",
+            "2",
+            "3",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: "Kyle",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbpnZg0AqhrNIAL4iIJdtUzrNWjR0A6orXvvoRAmo98bjyUSJ0",
+        scores: [
+            "3",
+            "2",
+            "5",
+            "4",
+            "1",
+            "3",
+            "2",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: "Kenny (princess)",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV9OijzTYM4bpgHvR0CxZJQPFFuMf-EdfJsbBdDY4NR6o8aHy8",
+        scores: [
+            "1",
+            "3",
+            "4",
+            "2",
+            "1",
+            "3",
+            "2",
+            "4",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Wendy",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAs7aQzlJhPB8AUFYG6GoLnH48OdtZ4YuOCYneA3pJyUrgMkpP",
+        scores: [
+            "5",
+            "3",
+            "4",
+            "4",
+            "5",
+            "2",
+            "4",
+            "4",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "THEY TERK ERR JERBS",
+        photo: "http://tse2.mm.bing.net/th?id=OIP.FxyqHHpsWMfjVX-ToApZZQHaEb",
+        scores: [
+            "2",
+            "3",
+            "2",
+            "3",
+            "2",
+            "3",
+            "2",
+            "3",
+            "2",
+            "3"
+
+        ]
+    },
+    {
+        name: "Chef",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlpnA7f7YjvK8Mhk8sPbNeDppjQnHxWlIqOgaf_gzqgTxuqz2P",
+        scores: [
+            "1",
+            "2",
+            "1",
+            "2",
+            "1",
+            "2",
+            "1",
+            "2",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Towelie",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4hsFv1YR5gJNPSIfzQpD6KGcMWdPpkfkAV4ayskiR0xmdR1-0",
+        scores: [
+            "4",
+            "5",
+            "4",
+            "5",
+            "4",
+            "5",
+            "4",
+            "5",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Jesus Christ",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6S9xn-F2sBeDof1vZo_VFwFIX3kgUhyUIOiIEztbstscMlpjQ",
+        scores: [
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
+
+module.exports = friends;
